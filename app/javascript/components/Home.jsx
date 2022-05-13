9cf74325deccec0a1cf6747928aee22538836b65
Rafael Garin <rafael@buda.com>
2022-05-12 20:29:02 -0400

    feat: implement local tasks component

--- app/javascript/components/Home.jsx
+++ app/javascript/components/Home.jsx
@@ -1,15 +1,46 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-// https://reactjs.org/docs/hooks-intro.html
-export default function () {
-  const [count, setCount] = useState(0);
+export default () => {
+  const [tasks, setTasks] = useState([]);
+  const [inputValue, setInputValue] = useState('');
+
+  function addTask () {
+    const newTask = {
+      description: inputValue,
+      id: tasks.length + 1,
+    }
+    const newTasks = tasks.concat([newTask]);
+    setTasks(newTasks);
+  }
+
+  function removeTask (id) {
+    const newTasks = tasks.filter((task) => (
+      task.id !== id
+    ));
+    setTasks(newTasks);
+  }
 
   return (
-    <div>
-      <p>You clicked {count} times</p>
-      <button onClick={() => setCount(count + 1)}>
-        Click me
-      </button>
+    <div className="main-container">
+      <h1 className="title">Tareas</h1>
+
+      <div className="new-task">
+        <label className="label">Nueva tarea:</label><br></br>
+        <input className="task-input" type="text" onChange={(e) => setInputValue(e.target.value)} />
+        <button className="add-task" type="button" onClick={addTask}>Agregar</button>
+      </div>
+
+      { tasks.map((task) => (
+        <div key={task.id} className="task-container">
+          <div className="task-subcontainer">
+            <input type="checkbox" className="checkbox"/>
+            <p className="description">
+              { task.description }
+            </p>
+          </div>
+          <button className="remove-button" type="button" onClick={() => removeTask(task.id) }>x</button>
+        </div>
+      )) }
     </div>
   );
-}
+};
