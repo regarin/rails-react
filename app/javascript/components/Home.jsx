c87cd3cd2d075751ea4d237c1ea7ce3aac2edb5f
Rafael Garin <rafael@buda.com>
2022-05-27 15:41:06 -0400

    feat: use tailwind

--- app/javascript/components/Home.jsx
+++ app/javascript/components/Home.jsx
@@ -42,24 +42,24 @@ export default () => {
   }, []);
 
   return (
-    <div className="main-container">
-      <h1 className="title">Tareas</h1>
+    <div className="p-5">
+      <h1 className="text-4xl mb-6">Tareas</h1>
 
-      <div className="new-task">
-        <label className="label">Nueva tarea:</label><br></br>
-        <input className="task-input" type="text" onChange={(e) => setInputValue(e.target.value)} />
-        <button className="add-task" type="button" onClick={addTask}>Agregar</button>
+      <div className="mb-6">
+        <label className="">Nueva tarea:</label><br></br>
+        <input className="rounded-md border-gray-300 mr-2" type="text" onChange={(e) => setInputValue(e.target.value)} />
+        <button className="rounded-md bg-blue-500 hover:bg-blue-600 text-white px-4 py-2" type="button" onClick={addTask}>Agregar</button>
       </div>
 
       { tasks.map((task) => (
-        <div key={task.id} className="task-container">
-          <div className="task-subcontainer">
-            <input type="checkbox" className="checkbox"/>
-            <p className="description">
+        <div key={task.id} className="flex flex-row justify-between hover:bg-gray-100 rounded-md px-4 py-2 items-center max-w-md">
+          <div className="flex flex-row items-center">
+            <input type="checkbox" className="mr-3 rounded-sm"/>
+            <p className="text-gray-700">
               { task.description }
             </p>
           </div>
-          <button className="remove-button" type="button" onClick={() => removeTask(task.id) }>x</button>
+          <button className="" type="button" onClick={() => removeTask(task.id) }>x</button>
         </div>
       )) }
     </div>
